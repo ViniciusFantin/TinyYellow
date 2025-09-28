5067f8205602d058a1b5f42d49e9f6e887603bab
Katun0 <joao_katun0@hotmail.com>
2025-09-28 18:20:32 -0300

    teste create post

--- tyellow/src/pages/CreatePost/CreatePost.js
+++ tyellow/src/pages/CreatePost/CreatePost.js
@@ -15,7 +15,7 @@ const CreatePost = () => {
   const [body, setBody] = useState("");
   const [tags, setTags] = useState("");
   const [formError, setFormError] = useState("");
-  const API_BASE = process.env.REACT_APP_API_BASE || "https://tiny-yellow-1et1.vercel.app";
+  const API_BASE = process.env.REACT_APP_API_BASE || "https://tiny-yellow-1et1.vercel.app/api/";
   const [loading, setLoading] = useState(false);
 
   const { user } = useAuthValue();
